3b37bdf90a360cb029ab9856db7563570d80fdf5
Sergey Shulga <shul.sergio@gmail.com>
2024-09-22 21:39:55 +0300

    v 1.4 add createSelector

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,21 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
-import { selectContacts, selectFilter } from "../../redux/selectors";
+import {
+  // selectContacts,
+  // selectFilter,
+  selectFilteredContacts,
+} from "../../redux/selectors";
 import { deleteContact } from "../../redux/contactsOps";
 
 export default function ContactList() {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-  console.log("contacts in ContactList:");
-  console.log(contacts);
-  console.log("filter in ContactList:");
-  console.log(filter);
-  const visibleContacts = contacts.filter(({ name }) =>
-    name.toLowerCase().includes(filter.toLowerCase())
-  );
-
+  // const contacts = useSelector(selectContacts);
+  // const filter = useSelector(selectFilter);
+  // console.log("contacts in ContactList:");
+  // console.log(contacts);
+  // console.log("filter in ContactList:");
+  // console.log(filter);
+  // const visibleContacts = contacts.filter(({ name }) =>
+  //   name.toLowerCase().includes(filter.toLowerCase())
+  // );
+  const visibleContacts = useSelector(selectFilteredContacts);
   const onDelete = (id) => {
     dispatch(deleteContact(id));
   };
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,22 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const selectIsLoading = (state) => state.contacts.loading;
 export const selectError = (state) => state.contacts.error;
 export const selectContacts = (state) => state.contacts.items;
 export const selectFilter = (state) => state.filters.name;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    //   const contacts = selectContacts(state);
+    //   const filter = selectFilter(state);
+    console.log("contacts in ContactList:");
+    console.log(contacts);
+    console.log("filter in ContactList:");
+    console.log(filter);
+
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
